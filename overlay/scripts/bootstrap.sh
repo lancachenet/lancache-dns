eeed1540982d3290799a90fd5bc510cf6363b933
Robin Lewis <me@gotenxiao.co.uk>
2016-02-29 20:58:42 +0000

    Add basic error handling to bootstrap.sh

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ -z "$STEAMCACHE_IP" ]
+then
+	echo "No value in \$STEAMCACHE_IP!" >&2
+	exit 1
+fi
+
 cp /etc/bind/steamcache/template.db.content_.steampowered.com /etc/bind/steamcache/db.content_.steampowered.com
 cp /etc/bind/steamcache/template.db.cs.steampowered.com /etc/bind/steamcache/db.cs.steampowered.com
 
