7a334ae3344d09a8499eab4c3778dd3553275041
Robin Lewis <me@gotenxiao.co.uk>
2016-02-29 20:59:49 +0000

    Allow resolving a hostname to an IP at startup

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if [ -n "$STEAMCACHE_RESOLVE_NAME" ]
+then
+	RESOLVED_IP="$(nslookup "$STEAMCACHE_RESOLVE_NAME" 2>/dev/null | grep 'Address' | awk '{ print $3 }')"
+	if [ -n "$RESOLVED_IP" ]
+	then
+		echo "Resolved ${STEAMCACHE_RESOLVE_NAME} to ${RESOLVED_IP}"
+		STEAMCACHE_IP="$RESOLVED_IP"
+	else
+		echo "Failed to resolve ${STEAMCACHE_RESOLVE_NAME}; using ${STEAMCACHE_IP} instead" >&2
+	fi
+fi
+
 if [ -z "$STEAMCACHE_IP" ]
 then
 	echo "No value in \$STEAMCACHE_IP!" >&2
