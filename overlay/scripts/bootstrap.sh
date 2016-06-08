8fc65834f297daf34de8c876c1d62ee137ca9545
Pips <gamerpips@gmail.com>
2016-06-07 21:04:17 -0600

    Update bootstrap.sh

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-if [ -n "$STEAMCACHE_RESOLVE_NAME" ]
-then
-	RESOLVED_IP="$(nslookup "$STEAMCACHE_RESOLVE_NAME" 2>/dev/null | grep 'Address' | awk '{ print $3 }')"
-	if [ -n "$RESOLVED_IP" ]
-	then
-		echo "Resolved ${STEAMCACHE_RESOLVE_NAME} to ${RESOLVED_IP}"
-		STEAMCACHE_IP="$RESOLVED_IP"
-	else
-		echo "Failed to resolve ${STEAMCACHE_RESOLVE_NAME}; using ${STEAMCACHE_IP} instead" >&2
-	fi
-fi
+# if [ -n "$STEAMCACHE_RESOLVE_NAME" ]
+# then
+# 	RESOLVED_IP="$(nslookup "$STEAMCACHE_RESOLVE_NAME" 2>/dev/null | grep 'Address' | awk '{ print $3 }')"
+# 	if [ -n "$RESOLVED_IP" ]
+# 	then
+# 		echo "Resolved ${STEAMCACHE_RESOLVE_NAME} to ${RESOLVED_IP}"
+# 		STEAMCACHE_IP="$RESOLVED_IP"
+# 	else
+# 		echo "Failed to resolve ${STEAMCACHE_RESOLVE_NAME}; using ${STEAMCACHE_IP} instead" >&2
+# 	fi
+# fi
 
-if [ -z "$STEAMCACHE_IP" ]
-then
-	echo "No value in \$STEAMCACHE_IP!" >&2
-	exit 1
-fi
+# if [ -z "$STEAMCACHE_IP" ]
+# then
+# 	echo "No value in \$STEAMCACHE_IP!" >&2
+# 	exit 1
+# fi
 
 echo "Running bootstrap.sh..."
 
