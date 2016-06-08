56b8afbbfef34923fd9c95aaae0da7f2381578f4
Robin Lewis <me@gotenxiao.co.uk>
2016-06-08 22:15:17 +0100

    Add support for steamcontent.com and steampipe.steamcontent.com

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -20,9 +20,11 @@ fi
 
 cp /etc/bind/steamcache/template.db.content_.steampowered.com /etc/bind/steamcache/db.content_.steampowered.com
 cp /etc/bind/steamcache/template.db.cs.steampowered.com /etc/bind/steamcache/db.cs.steampowered.com
+cp /etc/bind/steamcache/template.db.steamcontent.com /etc/bind/steamcache/db.steamcontent.com
 
 sed -i -e "s%{{ steamcache_ip }}%$STEAMCACHE_IP%g" /etc/bind/steamcache/db.content_.steampowered.com
 sed -i -e "s%{{ steamcache_ip }}%$STEAMCACHE_IP%g" /etc/bind/steamcache/db.cs.steampowered.com
+sed -i -e "s%{{ steamcache_ip }}%$STEAMCACHE_IP%g" /etc/bind/steamcache/db.steamcontent.com
 
 /usr/sbin/named -u named -c /etc/bind/named.conf -f
 
