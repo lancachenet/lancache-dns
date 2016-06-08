1ec4833488ae3edd59f85c04efffd3d02bc4115b
Pips <gamerpips@gmail.com>
2016-06-07 19:30:18 -0600

    Update bootstrap.sh

--- overlay/scripts/bootstrap.sh
+++ overlay/scripts/bootstrap.sh
@@ -18,15 +18,36 @@ then
 	exit 1
 fi
 
-cp /etc/bind/steamcache/template.db.content_.steampowered.com /etc/bind/steamcache/db.content_.steampowered.com
-cp /etc/bind/steamcache/template.db.cs.steampowered.com /etc/bind/steamcache/db.cs.steampowered.com
-cp /etc/bind/steamcache/template.db.lancache /etc/bind/steamcache/db.lancache
+## blizzard
+cp /etc/bind/cache/blizzard/template.db.blizzard /etc/bind/cache/blizzard/db.blizzard
+sed -i -e "s%{{ blizzardcache_ip }}%$BLIZZARDCACHE_IP%g" /etc/bind/cache/blizzard/db.blizzard
 
-sed -i -e "s%{{ steamcache_ip }}%$STEAMCACHE_IP%g" /etc/bind/steamcache/db.content_.steampowered.com
-sed -i -e "s%{{ steamcache_ip }}%$STEAMCACHE_IP%g" /etc/bind/steamcache/db.cs.steampowered.com
-sed -i -e "s%{{ lancache_ip }}%$LANCACHE_IP%g" /etc/bind/steamcache/db.lancache
+## frontier
+cp /etc/bind/cache/frontier/template.db.frontier /etc/bind/cache/frontier/db.frontier
+sed -i -e "s%{{ frontiercache_ip }}%$FRONTIERCACHE_IP%g" /etc/bind/cache/frontier/db.frontier
+
+## origin
+cp /etc/bind/cache/origin/template.db.origin /etc/bind/cache/origin/db.origin
+sed -i -e "s%{{ origincache_ip }}%$ORIGINCACHE_IP%g" /etc/bind/cache/origin/db.origin
+
+## riot
+cp /etc/bind/cache/riot/template.db.riot /etc/bind/cache/riot/db.riot
+sed -i -e "s%{{ riotcache_ip }}%$RIOTCACHE_IP%g" /etc/bind/cache/riot/db.riot
+
+## steam
+cp /etc/bind/cache/steam/template.db.content_.steampowered.com /etc/bind/cache/steam/db.content_.steampowered.com
+cp /etc/bind/cache/steam/template.db.cs.steampowered.com /etc/bind/cache/steam/db.cs.steampowered.com
+sed -i -e "s%{{ steamcache_ip }}%$STEAMCACHE_IP%g" /etc/bind/cache/steam/db.content_.steampowered.com
+sed -i -e "s%{{ steamcache_ip }}%$STEAMCACHE_IP%g" /etc/bind/cache/steam/db.cs.steampowered.com
+
+## uplay
+cp /etc/bind/cache/uplay/template.db.uplay /etc/bind/cache/uplay/db.uplay
+sed -i -e "s%{{ uplaycache_ip }}%$UPLAYCACHE_IP%g" /etc/bind/cache/uplay/db.uplay
+
+## windows
+cp /etc/bind/cache/windows/template.db.windows /etc/bind/cache/windows/db.windows
+sed -i -e "s%{{ windowscache_ip }}%$WINDOWSCACHE_IP%g" /etc/bind/cache/windows/db.windows
 
 /usr/sbin/named -u named -c /etc/bind/named.conf -f
-
 
 sleep 10000
