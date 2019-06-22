21dc4981052da26e778c8fe0c46d97ef7ad512ce
VibroAxe <vibs@macrolevel.co.uk>
2019-06-22 20:23:35 +0000

    Migrated to supervisord

--- Dockerfile
+++ Dockerfile
@@ -2,17 +2,16 @@ FROM lancachenet/ubuntu:latest
 MAINTAINER LanCache.Net Team <team@lancache.net>
 
 ENV STEAMCACHE_DNS_VERSION=1 ENABLE_DNSSEC_VALIDATION=false LANCACHE_DNSDOMAIN=cache.lancache.net
-RUN apt-get update && apt-get install -y bind jq curl
+RUN apt-get update && apt-get install -y bind9 jq curl dnsutils
 
 COPY	overlay/ /
 
 RUN	mkdir -p /var/cache/bind /var/log/named		\
 	&& chmod 755 /scripts/*				\
-	&& chown named:named /var/cache/bind /var/log/named
+	&& chown bind:bind /var/cache/bind /var/log/named
 
 
 EXPOSE 53/udp
 
 WORKDIR /scripts
 
-CMD ["bash", "/scripts/bootstrap.sh"]
---
+++ overlay/hooks/entrypoint-pre.d/10_generate_config.sh
@@ -0,0 +1,166 @@
+#/bin/bash
+
+set -e
+
+ZONEPATH="/etc/bind/cache/"
+ZONETEMPLATE="/etc/bind/cache/zone.tmpl"
+CACHECONF="/etc/bind/cache.conf"
+USE_GENERIC_CACHE="${USE_GENERIC_CACHE:-false}"
+LANCACHE_DNSDOMAIN="${LANCACHE_DNSDOMAIN:-cache.lancache.net}"
+CACHE_ZONE="${ZONEPATH}$LANCACHE_DNSDOMAIN.db"
+RPZ_ZONE="${ZONEPATH}rpz.db"
+
+echo "     _                                      _                       _   "
+echo "    | |                                    | |                     | |  "
+echo " ___| |_ ___  __ _ _ __ ___   ___ __ _  ___| |__   ___   _ __   ___| |_ "
+echo "/ __| __/ _ \\/ _\` | '_ \` _ \\ / __/ _\` |/ __| '_ \\ / _ \\ | '_ \\ / _ \\ __|"
+echo "\\__ \\ ||  __/ (_| | | | | | | (_| (_| | (__| | | |  __/_| | | |  __/ |_ "
+echo "|___/\\__\\___|\\__,_|_| |_| |_|\\___\\__,_|\\___|_| |_|\\___(_)_| |_|\\___|\\__|"
+echo ""
+echo ""
+if ! [ -z "${UPSTREAM_DNS}" ] ; then
+  echo "configuring /etc/resolv.conf to stop from looping to ourself"
+  echo "nameserver ${UPSTREAM_DNS}" > /etc/resolv.conf
+fi
+echo ""
+
+
+
+if [ "$USE_GENERIC_CACHE" = "true" ]; then
+  if [ -z ${LANCACHE_IP} ]; then
+    echo "If you are using USE_GENERIC_CACHE then you must set LANCACHE_IP"
+    exit 1
+  fi
+else
+  if ! [ -z ${LANCACHE_IP} ]; then
+    echo "If you are using LANCACHE_IP then you must set USE_GENERIC_CACHE=true"
+    exit 1
+  fi
+fi
+
+echo "Bootstrapping DNS from https://github.com/uklans/cache-domains"
+
+if [ "$USE_GENERIC_CACHE" = "true" ]; then
+    echo ""
+    echo "----------------------------------------------------------------------"
+    echo "Using Generic Server: ${LANCACHE_IP}"
+    echo "Make sure you are using a monolithic cache or load balancer at ${LANCACHE_IP}"
+    echo "----------------------------------------------------------------------"
+    echo ""
+fi
+
+rm -f ${CACHECONF}
+touch ${CACHECONF}
+
+#Add the rpz zones to the cache.conf
+echo "
+	zone \"$LANCACHE_DNSDOMAIN\" {
+		type master;
+		file \"$CACHE_ZONE\";
+	};
+    zone \"rpz\" {
+      type master;
+      file \"$RPZ_ZONE\";
+      allow-query { none; };
+    };" > ${CACHECONF}
+
+#Generate the SOA for cache.lancache.net
+
+echo "\$ORIGIN $LANCACHE_DNSDOMAIN. 
+\$TTL    600
+@       IN  SOA localhost. dns.lancache.net. (
+             $(date +%s)
+             604800
+             600
+             600
+             600 )
+@       IN  NS  localhost.
+
+" > $CACHE_ZONE
+
+#Generate the RPZ zone file
+
+echo "\$TTL 60
+@            IN    SOA  localhost. root.localhost.  (
+                          2   ; serial 
+                          3H  ; refresh 
+                          1H  ; retry 
+                          1W  ; expiry 
+                          1H) ; minimum 
+                  IN    NS    localhost." > $RPZ_ZONE
+
+curl -s -o services.json https://raw.githubusercontent.com/uklans/cache-domains/master/cache_domains.json
+
+cat services.json | jq -r '.cache_domains[] | .name, .domain_files[]' | while read L; do
+  if ! echo ${L} | grep "\.txt" >/dev/null 2>&1 ; then
+    SERVICE=${L}
+    # Uppercase service, strip non-alphanumeric characters and replace with underscores
+    SERVICEUC=`echo ${L} | sed 's/[^a-z0-9]\+/_/g' | tr [:lower:] [:upper:]`
+	echo "Processing service: $SERVICE"
+	CONTINUE=false
+	SERVICE_ENABLED=false
+	if [ "$USE_GENERIC_CACHE" = "true" ]; then
+    	if ! env | grep "DISABLE_${SERVICEUC}=true" >/dev/null 2>&1; then
+			SERVICE_ENABLED=true	
+		fi
+	else
+		echo "testing for presence of ${SERVICEUC}CACHE_IP"
+    	if env | grep "${SERVICEUC}CACHE_IP" >/dev/null 2>&1; then
+			SERVICE_ENABLED=true
+		fi
+	fi
+	if [ "$SERVICE_ENABLED" == "true" ]; then
+    	if env | grep "${SERVICEUC}CACHE_IP" >/dev/null 2>&1; then
+    		C_IP=$(env | grep "${SERVICEUC}CACHE_IP=" | sed 's/.*=//')
+    	else
+    		C_IP=${LANCACHE_IP}
+    	fi
+		if [ "x$C_IP" != "x" ]; then
+			echo "Enabling service with ip(s): $C_IP";
+			for IP in $C_IP; do
+				echo "$SERVICE IN A $IP;" >> $CACHE_ZONE
+			done
+      		echo ";## ${SERVICE}" >> ${RPZ_ZONE}
+			CONTINUE=true
+		else
+			echo "Could not find IP for requested service: $SERVICE"
+			exit 1
+		fi
+	else
+		echo "Skipping $SERVICE"
+	fi
+
+  else
+	if [ "$CONTINUE" == "true" ]; then
+
+      curl -s -o ${L} https://raw.githubusercontent.com/uklans/cache-domains/master/${L}
+    	## files don't have a newline at the end
+    	echo "" >> ${L}
+		cat ${L} | grep -v "^#" | while read URL; do
+      		if [ "x${URL}" != "x" ] ; then
+				#RPZ entries do NOT need a trailing . on the rpz domain, but do for the redirect host
+				echo "${URL} IN CNAME $SERVICE.$LANCACHE_DNSDOMAIN.;" >> $RPZ_ZONE;
+      		fi
+    	done
+      rm ${L}
+    fi
+  fi
+done
+
+rm services.json
+
+echo ""
+echo " --- "
+echo ""
+
+if ! [ -z "${UPSTREAM_DNS}" ] ; then
+  sed -i "s/#ENABLE_UPSTREAM_DNS#//;s/dns_ip/${UPSTREAM_DNS}/" /etc/bind/named.conf.options
+fi
+
+if [ "${ENABLE_DNSSEC_VALIDATION}" = true ] ; then
+	echo "Enabling dnssec validation"
+	sed -i "s/dnssec-validation no/dnssec-validation auto/" /etc/bind/named.conf.options
+fi
+
+echo "finished bootstrapping."
+
