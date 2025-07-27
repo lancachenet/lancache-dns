3bc8d68b4025b93b0ff4d7763ee14dbedb8d6b1f
Amir Zarrinkafsh <nightah@me.com>
2025-07-27 20:39:25 +1000

    refactor: dockerfile adjustments, add lefthook and megalinter
    This change adjusts the `Dockerfile` with enhancements only available in newer Docker and Buildx releases.
    
    I have also added LeftHook and MegaLinter with an appropriately adjusted configuration for linting.

--- overlay/hooks/entrypoint-pre.d/10_generate_config.sh
+++ overlay/hooks/entrypoint-pre.d/10_generate_config.sh
@@ -1,4 +1,3 @@
-#/bin/bash
+#!/bin/bash
 
 /usr/local/bin/dnstool generate lancache-dns
-
--- run-tests.sh
+++ run-tests.sh
@@ -1,60 +1,57 @@
 #!/bin/bash
-which goss
 
-if [ $? -ne 0 ]; then
+if ! command -v goss >/dev/null 2>&1; then
 	echo "Please install goss from https://goss.rocks/install"
 	echo "For a quick auto install run the following"
 	echo "curl -fsSL https://goss.rocks/install | sh"
-	exit $?
+	exit 1
 fi
 
-GOSS_WAIT_OPS="-r 60s -s 1s"
-
 docker build --tag lancachenet/lancache-dns:goss-test .
-case $1 in
-  circleci)
-    shift;
-    mkdir -p ./reports/goss
-	if [[ "$1" == "keepimage" ]]; then
+case ${1} in
+circleci)
+	shift
+	mkdir -p ./reports/goss
+	if [[ "${1}" == "keepimage" ]]; then
 		KEEPIMAGE=true
 		shift
 	fi
-    export GOSS_OPTS="$GOSS_OPTS --format junit"
-	dgoss run -e USE_GENERIC_CACHE=true -e STEAMCACHE_IP=10.0.0.2 -e LANCACHE_IP=10.0.0.1 -e UPSTREAM_DNS="8.8.8.8; 1.1.1.1" $@ lancachenet/lancache-dns:goss-test > reports/goss/report.xml
+	export GOSS_OPTS="${GOSS_OPTS} --format junit"
+	dgoss run -e USE_GENERIC_CACHE=true -e STEAMCACHE_IP=10.0.0.2 -e LANCACHE_IP=10.0.0.1 -e UPSTREAM_DNS="8.8.8.8; 1.1.1.1" "$@" lancachenet/lancache-dns:goss-test >reports/goss/report.xml
 	#store result for exit code
 	RESULT=$?
 	#delete the junk that goss currently outputs :(
-    sed -i '0,/^</d' reports/goss/report.xml
+	sed -i '0,/^</d' reports/goss/report.xml
 	#remove invalid system-err outputs from junit output so circleci can read it
 	sed -i '/<system-err>.*<\/system-err>/d' reports/goss/report.xml
-    ;;
-  docker)
-	shift;
-	if [[ "$1" == "keepimage" ]]; then
+	;;
+docker)
+	shift
+	if [[ "${1}" == "keepimage" ]]; then
 		KEEPIMAGE=true
 		shift
 	fi
-	dgoss edit -e USE_GENERIC_CACHE=true -e STEAMCACHE_IP=10.0.0.2 -e LANCACHE_IP=10.0.0.1 -e UPSTREAM_DNS="8.8.8.8; 1.1.1.1" $@ lancachenet/lancache-dns:goss-test
+	dgoss edit -e USE_GENERIC_CACHE=true -e STEAMCACHE_IP=10.0.0.2 -e LANCACHE_IP=10.0.0.1 -e UPSTREAM_DNS="8.8.8.8; 1.1.1.1" "$@" lancachenet/lancache-dns:goss-test
 	RESULT=$?
-    ;;
-  edit)
-	shift;
-	if [[ "$1" == "keepimage" ]]; then
+	;;
+edit)
+	shift
+	if [[ "${1}" == "keepimage" ]]; then
 		KEEPIMAGE=true
 		shift
 	fi
-	dgoss edit -e USE_GENERIC_CACHE=true -e STEAMCACHE_IP=10.0.0.2 -e LANCACHE_IP=10.0.0.1 -e UPSTREAM_DNS="8.8.8.8; 1.1.1.1" $@ lancachenet/lancache-dns:goss-test
+	dgoss edit -e USE_GENERIC_CACHE=true -e STEAMCACHE_IP=10.0.0.2 -e LANCACHE_IP=10.0.0.1 -e UPSTREAM_DNS="8.8.8.8; 1.1.1.1" "$@" lancachenet/lancache-dns:goss-test
 	RESULT=$?
-    ;;
-  *)
-	if [[ "$1" == "keepimage" ]]; then
+	;;
+*)
+	if [[ "${1}" == "keepimage" ]]; then
 		KEEPIMAGE=true
 		shift
 	fi
-	dgoss run -e USE_GENERIC_CACHE=true -e STEAMCACHE_IP=10.0.0.2 -e LANCACHE_IP=10.0.0.1 -e UPSTREAM_DNS="8.8.8.8; 1.1.1.1" $@ lancachenet/lancache-dns:goss-test
+	dgoss run -e USE_GENERIC_CACHE=true -e STEAMCACHE_IP=10.0.0.2 -e LANCACHE_IP=10.0.0.1 -e UPSTREAM_DNS="8.8.8.8; 1.1.1.1" "$@" lancachenet/lancache-dns:goss-test
 	RESULT=$?
-    ;;
+	;;
 esac
-[[ "$KEEPIMAGE" == "true" ]] || docker rmi lancachenet/lancache-dns:goss-test
+[[ "${KEEPIMAGE}" == "true" ]] || docker rmi lancachenet/lancache-dns:goss-test
 
 exit $RESULT
